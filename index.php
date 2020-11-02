ce278de1c1c00a302ee6f4c1d363b511a4fb823f
Joseph <softjoe40@gmail.com>
2020-11-02 17:10:50 +0100

    Application crud is ready

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
-<?php  include('server.php'); ?>
+<?php  
+
+include('server.php'); 
+
+	if (isset($_GET['edit'])) {
+		$id = $_GET['edit'];
+		$update = true;
+		$record = mysqli_query($db, "SELECT * FROM info WHERE id=$id");
+
+		if (mysqli_num_rows($record) == 1 ) {
+			$n = mysqli_fetch_array($record);
+			$name = $n['name'];
+			$address = $n['address'];
+		}
+	}
+
+
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,18 +24,58 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+    <?php $results = mysqli_query($db, "SELECT * FROM info"); ?>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Address</th>
+                <th>Time Created</th>
+                <th colspan="2">Action</th>
+            </tr>
+        </thead>
+        
+        <?php while ($row = mysqli_fetch_array($results)) { ?>
+            <tr>
+                <td><?php echo $row['name']; ?></td>
+                <td><?php echo $row['address']; ?></td>
+                <td><?php echo $row['created_at']; ?></td>
+                <td>
+                    <a href="index.php?edit=<?php echo $row['id']; ?>" class="edit_btn" >Edit</a>
+                </td>
+                <td>
+                    <a href="server.php?del=<?php echo $row['id']; ?>" class="del_btn">Delete</a>
+                </td>
+            </tr>
+        <?php } ?>
+    </table>
 	<form method="post" action="server.php" >
 		<div class="input-group">
 			<label>Name</label>
-			<input type="text" name="name" value="">
+			<input type="text" name="name" value="<?php echo $name; ?>">
 		</div>
 		<div class="input-group">
 			<label>Address</label>
-			<input type="text" name="address" value="">
+			<input type="text" name="address" value="<?php echo $address; ?>">
 		</div>
 		<div class="input-group">
-			<button class="btn" type="submit" name="save" >Save</button>
+            <input type="hidden" name="id" value="<?php echo $id; ?>">
+			<?php if ($update == true): ?>
+                <button class="btn" type="submit" name="update" style="background: #556B2F;" >update</button>
+            <?php else: ?>
+                <button class="btn" type="submit" name="save" >Save</button>
+            <?php endif ?>
 		</div>
 	</form>
 </body>
+<body>
+<?php if (isset($_SESSION['message'])): ?>
+	<div class="msg">
+		<?php 
+			echo $_SESSION['message']; 
+			unset($_SESSION['message']);
+		?>
+	</div>
+<?php endif ?>
 </html>
